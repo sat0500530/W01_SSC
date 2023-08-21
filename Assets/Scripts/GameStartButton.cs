3c792690d81d5eae91a0decdbf149c88aead1463
sat0500530 <sat0500530@gmail.com>
2023-08-21 17:40:15 +0900

    Add_TitleScene
    Add_TitleScene

--- Assets/Scripts/GameStartButton.cs
+++ Assets/Scripts/GameStartButton.cs
@@ -2,7 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameStartButton : MonoBehaviour
 {
+    public bool goToTitle = false;
+    private void Awake()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(GameStart);
+    }
+
+    void GameStart()
+    {
+        SceneManager.LoadScene(goToTitle ? "Title" : "InGame");
+    }
 }
